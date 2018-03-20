9042ab6aea2ac6f953f2d0c264eff8ef335c6e5e
firezdog@yahoo.com <firezdog@yahoo.com>
2018-03-20 10:47:52 -0500

    ...

--- sports_orm/apps/leagues/views.py
+++ sports_orm/apps/leagues/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
+from django.db.models import Count
 
 from . import team_maker
 
 def index(request):
 	context = {
-        "leagues": League.objects.all(),
-        "teams": Team.objects.all(),
-        "players": Player.objects.filter(first_name = "Alexander") | Player.objects.filter(first_name = "Wyatt"),
-    }
-	
+		"leagues": League.objects.all(),
+		"teams": Team.objects.annotate(num_players = Count('all_players')).filter(num_players__gt = 12),
+		"players": Player.objects.annotate(num_teams = Count('all_teams')).order_by('num_teams')
+	}
+
 	return render(request, "leagues/index.html", context)
 
 def make_data(request):
